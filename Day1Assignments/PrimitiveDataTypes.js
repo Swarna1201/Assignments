c3133370f6d0de419e60b095df80a575943ea89e
Swarna_lakshmi <123090518+Swarna1201@users.noreply.github.com>
2024-12-08 00:31:24 +0530

    Add files via upload
    Day1 Assignment

---
+++ Day1Assignments/PrimitiveDataTypes.js
@@ -0,0 +1,21 @@
+/*Create the following variables using let (not using var) and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types*/
+
+let firstName = "Swarna Lakshmi";
+let companyName = 'Capgemini';
+let mobileNumber = +919789633273;
+let isAutomation = true;
+let hasPlaywright;
+
+console.log("Datatype of firstName is " + typeof firstName);
+console.log("Datatype of companyname is "+ typeof companyName);
+console.log("Datatype of mobileNumber is "+ typeof mobileNumber);
+console.log("Datatype of isAutomation is "+ typeof isAutomation);
+console.log("Datatype of hasPlaywright is "+ typeof hasPlaywright);
+
+
